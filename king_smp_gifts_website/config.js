916bd9cdf982966b4023791e4ab47c53318340f7
vinh033397-a11y <vinh033397@stu.vinschool.edu.vn>
2026-07-23 22:44:27 +0700

    Update config.js

--- king_smp_gifts_website/config.js
+++ king_smp_gifts_website/config.js
@@ -4,7 +4,7 @@
   +07:00 là múi giờ Việt Nam.
 */
 const GIFT_CONFIG = {
-  firstUnlockDate: "2026-07-23T00:00:00+07:00",
+  firstUnlockDate: "2026-07-24T00:00:00+07:00",
   totalDays: 16,
   owner: "Nguyễn Xuân Vinh",
   recipient: "Nhữ Đình Phong"
